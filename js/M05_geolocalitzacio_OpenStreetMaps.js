cdc1bbb0c86c8831909d07a5e14287de36fc0cdc
sergi grau <sergi.grau@fje.edu>
2026-01-16 15:48:20 +0100

    Add files via upload

--- js/M05_geolocalitzacio_OpenStreetMaps.js
+++ js/M05_geolocalitzacio_OpenStreetMaps.js
@@ -18,48 +18,54 @@
 
 
 
+
 function iniciar() {
   var view = new ol.View({
-    center: [0, 0],
+    center: ol.proj.fromLonLat([0, 0]),
     zoom: 2
   });
-  
+
   var map = new ol.Map({
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
       })
     ],
-    target: 'map',
-    controls: ol.control.defaults({
-      attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
-        collapsible: false
-      })
-    }),
+    target: 'mapa', // ha de coincidir amb l'id del div
     view: view
   });
-  
-  var geolocation = new ol.Geolocation({
-    trackingOptions: {
-      enableHighAccuracy: true,
-    },
-    projection: view.getProjection(),
-  });
-    
-  var accuracyFeature = new ol.Feature();
-  
-  var positionFeature = new ol.Feature();
-  positionFeature.bindTo('geometry', geolocation, 'position')
-      .transform(function() {}, function(coordinates) {
-        return coordinates ? new ol.geom.Point(coordinates) : null;
-      });
-  
-  var featuresOverlay = new ol.FeatureOverlay({
-    map: map,
-    features: [accuracyFeature, positionFeature]
-  });
-  
 
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      var coords = [pos.coords.longitude, pos.coords.latitude];
+      var position = ol.proj.fromLonLat(coords);
+      view.setCenter(position);
+      view.setZoom(15);
+
+      var marker = new ol.Feature({
+        geometry: new ol.geom.Point(position)
+      });
+      var vectorSource = new ol.source.Vector({
+        features: [marker]
+      });
+      var markerStyle = new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 7,
+          fill: new ol.style.Fill({ color: 'red' }),
+          stroke: new ol.style.Stroke({ color: 'white', width: 2 })
+        })
+      });
+      var vectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+        style: markerStyle
+      });
+      map.addLayer(vectorLayer);
+    }, function (err) {
+      alert('No s\'ha pogut obtenir la teva localització.');
+    });
+  } else {
+    alert('El teu navegador no suporta la geolocalització.');
+  }
 }
 
 
